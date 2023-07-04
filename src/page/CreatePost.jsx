d2759cd1f3f8280493467166b30de0d7d19fcb66
Dash10107 <dakshcjain@gmail.com>
2023-07-04 10:59:03 +0530

    Succesfully Passes Image to Backend

---
+++ src/page/CreatePost.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
+
+const CreatePost = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: null,
+  });
+  
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    if (e.target.name === 'photo') {
+      const file = e.target.files[0];
+      setForm({ ...form, photo: file });
+    } else {
+      setForm({ ...form, [e.target.name]: e.target.value });
+    }
+  };
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const formData = new FormData();
+        formData.append('name', form.name);
+        formData.append('prompt', form.prompt);
+        formData.append('photo', form.photo);
+
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          body: formData,
+        });
+
+      const result =   await response.json();
+      if(result.success===true){
+         alert('Successfully Posted');
+        navigate('/');
+      }else{
+        alert(result.message)
+      }
+
+       
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please provide both prompt and image');
+    }
+  };
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Choose Images and Share it with the community</p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Ex., john doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                 src={URL.createObjectURL(form.photo)}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+          </div>
+
+          <FormField
+            labelName="Image"
+            type="file"
+            name="photo"
+            accept="image/*"
+            handleChange={handleChange}
+          />
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have chosen the image you want, you can share it with others in the community **</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default CreatePost;
